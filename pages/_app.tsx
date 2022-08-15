23db8e53cd7ede2fc29d7ad30d199892d838391b
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-08-16 00:03:50 +0100

    Add MantineProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import {SessionProvider} from "next-auth/react";
+import {MantineProvider} from "@mantine/core";
+
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+          <MantineProvider
+              withGlobalStyles
+              withNormalizeCSS
+              theme={{
+                  colorScheme: "light",
+              }}
+          >
+            <Component {...pageProps} />
+          </MantineProvider>
       </SessionProvider>
   );
 }
