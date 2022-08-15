13e52b990e25cafee3a08411a4bb5202842fc504
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-08-15 19:10:23 +0100

    Let login buttons display user info on correct session

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -14,16 +14,16 @@ export const LoginButton: React.FC<ILoginButton> = ({ style }) => {
 
     console.log({ session })
 
-    if (session) {
-        if (style == 'discord') {
+    if (style == 'discord') {
+        if (session?.discord.tag) {
             return (
                 <>
                     <div className="h-11 max-w-300 border-4 border-green-600 rounded-md flex p-3 flex-row items-center justify-center m-2">
                         <div className='flex-none pr-2'>
                             <FontAwesomeIcon icon={faDiscord} className='text-[#5865F2] text-lg h-4 w-5'/>
                         </div>
-                        <div className='flex-auto'>
-                            {session?.user?.name}
+                        <div className='flex-auto grow-1'>
+                            {session.discord.tag}
                         </div>
                         <div className='flex-none pl-10' onClick={() => signOut()}>
                             <FontAwesomeIcon icon={faCircleCheck} className='text-green-600 text-sm h-5 w-6'/>
@@ -31,26 +31,7 @@ export const LoginButton: React.FC<ILoginButton> = ({ style }) => {
                     </div>
                 </>
             )
-        } else if (style == 'microsoft') {
-            return (
-                <>
-                    <div className="h-11 max-w-300 border-4 border-green-600 rounded-md flex p-3 flex-row items-center justify-center m-2">
-                        <div className='flex-none pr-2'>
-                            <FontAwesomeIcon icon={faMicrosoft} className='text-[#5865F2] text-lg h-4 w-5'/>
-                        </div>
-                        <div className='flex-auto'>
-                            {session?.user?.name}
-                        </div>
-                        <div className='flex-none pl-10' onClick={() => signOut()}>
-                            <FontAwesomeIcon icon={faCircleCheck} className='text-green-600 text-sm h-3 w-4'/>
-                        </div>
-                    </div>
-                </>
-            )
-        }
-
-    } else {
-        if (style == 'discord') {
+        } else {
             return (
                 <>
                     <div onClick={() => signIn('discord')} className="cursor-pointer h-11 w-64 text-white max-w-300 bg-[#5865F2] hover:bg-[#4351E3] rounded-md flex p-3 flex-row items-center justify-center m-2">
@@ -61,7 +42,28 @@ export const LoginButton: React.FC<ILoginButton> = ({ style }) => {
                     </div>
                 </>
             )
-        } else if (style == 'microsoft') {
+        }
+
+
+    } else if (style == 'microsoft') {
+        if (session?.microsoft.email) {
+            return (
+                <>
+                    <div className="h-11 max-w-300 grow-1 border-4 border-green-600 rounded-md flex p-3 flex-row items-center justify-center m-2">
+                        <div className='flex-none pr-2'>
+                            <FontAwesomeIcon icon={faMicrosoft} className='text-[#005C85] text-lg h-4 w-5'/>
+                        </div>
+                        <div className='flex-auto grow-1'>
+                            {session.microsoft.email}
+                        </div>
+                        <div className='flex-none pl-10' onClick={() => signOut()}>
+                            <FontAwesomeIcon icon={faCircleCheck} className='text-green-600 text-sm h-5 w-6'/>
+                        </div>
+                    </div>
+                </>
+            )
+
+        } else {
             return (
                 <>
                     <div onClick={() => signIn('azure-ad')} className="cursor-pointer h-11 w-64 text-white max-w-300 bg-[#005C85] hover:bg-[#024460] rounded-md flex p-3 flex-row items-center justify-center m-2">
@@ -73,6 +75,8 @@ export const LoginButton: React.FC<ILoginButton> = ({ style }) => {
                 </>
             )
         }
+
+
     }
     return <></>
 }
