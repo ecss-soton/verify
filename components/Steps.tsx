e3af1cda1978ea4080fe00791b2f488d5b56771f
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-08-15 19:07:25 +0100

    Add fully functional stepper

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stepper } from '@mantine/core';
+import {Button, Stepper } from '@mantine/core';
 import {LoginButton} from "@/components/LoginButton";
 import {useSession} from "next-auth/react";
 
@@ -7,8 +7,11 @@ export const Steps: React.FC = () => {
     let step = 0;
 
     const { data: session } = useSession();
-    if (session) {
-        step++;
+    if (session?.microsoft.email) {
+        step = 1;
+    }
+    if (session?.discord.tag) {
+        step = 2;
     }
 
     return (
@@ -20,10 +23,13 @@ export const Steps: React.FC = () => {
                     </div>
                 </Stepper.Step>
                 <Stepper.Step label="Sign in with Discord" description="Link your account" allowStepSelect={false}>
-                    <LoginButton style={'discord'}/>
+                    <div className="flex flex-col items-center justify-center w-full flex-1 px-40 text-center">
+                        <LoginButton style={'discord'}/>
+                    </div>
                 </Stepper.Step>
                 <Stepper.Completed>
                     Congratulations! You've just linked your Southampton uni account and discord account!
+                    <Button variant="light">Test</Button>
                 </Stepper.Completed>
             </Stepper>
         </>
