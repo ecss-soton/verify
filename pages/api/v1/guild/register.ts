011465d9853588c8c6b88391550c17dde2a1c0f1
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-08-17 22:00:09 +0100

    Create register.ts

---
+++ pages/api/v1/guild/register.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../prisma/client";
+import {auth} from "../../../../middleware/auth";
+import { randomUUID } from 'crypto'
+
+interface ResponseData {
+    registered: boolean
+    approved: boolean
+}
+
+interface ResponseError {
+    error: boolean
+    message: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData | ResponseError>
+) {
+    if (req.method !== "POST") return res.status(405);
+    const check = auth(req, res);
+    if (check) return res;
+
+
+    const checkGuild = await prisma.guild.findUnique({
+        where: {
+            id: req.body.guildId,
+        }
+    })
+
+    if (checkGuild?.approved == true) {
+        return res.status(409).json({
+            error: true,
+            message: 'This guild already exists and cannot be modified as it is an approved guild',
+        })
+    }
+
+    const guild = await prisma.guild.upsert({
+        create: {
+            id: req.body.guildId,
+            name: req.body.name,
+            icon: req.body.icon,
+            createdAt: req.body.createdAt,
+            ownerId: req.body.ownerId,
+            susuLink: req.body.susuLink,
+            roleId: req.body.roleId,
+            roleName: req.body.roleName,
+            roleColour: req.body.roleColour,
+            inviteLink: req.body.inviteLink,
+            memberCount: req.body.memberCount,
+            approved: false,
+            apiKey: randomUUID(),
+        },
+        update: {
+            name: req.body.name,
+        },
+        where: {
+            id: req.body.guildId,
+        }
+    })
+
+    res.status(200).json({
+        registered: true,
+        approved: guild.approved
+    });
+}
