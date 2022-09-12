fa2868d34c96be484a01f1de79bfee25ac49b0cc
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-09-12 18:39:15 +0100

    Convert from router to next/link

--- components/MangeLink.tsx
+++ components/MangeLink.tsx
@@ -2,13 +2,12 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import React from "react";
 import { Button } from '@mantine/core';
 import {LoginButton} from "@/components/LoginButton";
-import {useRouter} from "next/router";
+import Link from "next/link";
 
 export const MangeLink: React.FC = () => {
 
     const { data: session } = useSession();
 
-    const router = useRouter()
 
     return (
         <>
@@ -17,10 +16,14 @@ export const MangeLink: React.FC = () => {
                 <LoginButton style={'microsoft'} />
                 <div className="flex flex-row pt-6">
                     <div className="pr-1">
-                        <Button onClick={() => router.push('/manage')} className="" variant="filled">Manage verification</Button>
+                        <Link href="/manage" passHref>
+                            <Button className="" variant="filled">Manage verification</Button>
+                        </Link>
                     </div>
                     <div className="pl-1">
-                        <Button onClick={() => router.push('/audit')}  variant="outline">View audit log</Button>
+                        <Link href="/audit" passHref>
+                            <Button variant="outline">View audit log</Button>
+                        </Link>
                     </div>
                 </div>
             </div>
